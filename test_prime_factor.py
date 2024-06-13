490f932276b23ac3e425d280563fdcefe61db413
shpark <sh0913.park@samsung.com>
2024-06-13 10:08:43 +0900

    [feature ] 49

--- test_prime_factor.py
+++ test_prime_factor.py
@@ -24,3 +24,6 @@ class TestPrimeFactor(TestCase):
 
     def test_prime_factor_of_9(self):
         self.assertEqual(self.prime_factor.of(9), [3, 3])
+
+    def test_prime_factor_of_12(self):
+        self.assertEqual(self.prime_factor.of(12), [2, 2, 3])
